53b313b8168bfb05335b68103f2b59fe209db174
Dishapatel <dmp89@njit.edu>
2022-10-06 18:05:48 -0400

    4th Update

--- IT490-Composer-Script.sh
+++ IT490-Composer-Script.sh
@@ -2,7 +2,7 @@
 echo "Working..."
 echo "Sening to Youssef's device"
 ssh youssef@192.168.232.128
-ssh victoryajayi@44.211.175.142
+ssh victoryajayi@172.31.80.134
 echo "abcd"
 sudo systemctl status apache2
 sudo systemctl start apache2
