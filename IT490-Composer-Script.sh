66530e87007b9acbea08093ac3c9f56c2dc9408c
Dishapatel <dmp89@njit.edu>
2022-10-06 17:59:48 -0400

    Update Composer

--- IT490-Composer-Script.sh
+++ IT490-Composer-Script.sh
@@ -2,7 +2,7 @@
 echo "Working..."
 echo "Sening to Youssef's device"
 ssh youssef@192.168.232.128
-ssh vickyajayi@44.211.175.142
+ssh victoryajayi@44.211.175.142
 echo "abcd"
 sudo systemctl status apache2
 sudo systemctl start apache2
--- readandwritefromqueue.py
+++ readandwritefromqueue.py
@@ -1,3 +1,5 @@
+import Queue #
+
 
 queue = []
 queue.append("data")
@@ -7,4 +9,16 @@ print(queue)
 queue.pop(0)
 print(queue)
 
+Class Queue:
 
+	def _init_(self):
+            self.queue = []
+        def enqueue(self, item):
+            self.queue.append(item)
+        def dequeue(self):
+            if len(self.queue) < 1:
+            	return None
+            return self.queue.pop(0)
+        def size(self):
+            return len(self.queue)
+       
