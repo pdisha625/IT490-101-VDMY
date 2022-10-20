74b7f1f1e06dd1c96add864204ad0f0c9718b51d
DishaPatel <dmp89@njit.edu>
2022-10-20 14:50:10 -0400

    send.py

--- Composer.sh
+++ Composer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -t -p 22 os95@10.242.125.55 'bash -s < /home/os95/IT490-101-VDMY/rabbitmqScript'
+ssh -t -p 22 youssef@10.242.36.102 'bash -s < /home/youssef/Desktop/IT490/IT490-101-VDMY/RabbitmqScript.sh'
 
 ssh -t -p 22 mohamed11@10.242.192.211 'bash -s < /home/mohamed11/Desktop/IT490/IT490-101-VDMY/MYSQLcomposer.sh'
 
