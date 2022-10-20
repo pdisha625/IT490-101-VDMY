a193da1168edc42ba281d071f17d23701521efb0
DishaPatel <dmp89@njit.edu>
2022-10-19 22:42:27 -0400

    it490

--- Composer.sh
+++ Composer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-ssh -t -p 22 youssef@10.242.36.102 
-./RabbitmqScript.sh
+ssh -t -p 22 os95@10.242.125.55 'bash -s < /home/os95/IT490-101-VDMY/rabbitmqScript'
 
-ssh -t -p 22 mohamed11@10.242.192.211
-./sqlcomposer.sh 
+ssh -t -p 22 mohamed11@10.242.192.211 'bash -s < /home/mohamed11/Desktop/IT490/IT490-101-VDMY/MYSQLcomposer.sh'
 
-ssh -t -p 22 victoryajayi@10.242.49.24
-./apachecomposer.sh
+ssh -t -p 22 victoryajayi@10.242.49.24 'bash -s < /home/victoryajayi/IT490-101-VDMY/apacheComposer'
+
 
