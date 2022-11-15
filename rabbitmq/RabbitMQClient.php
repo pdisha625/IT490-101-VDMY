a2a73b42973e27993062621f355ce878320efb76
DishaPatel <dmp89@njit.edu>
2022-11-14 19:29:37 -0500

    MTDeliver

--- rabbitmq/RabbitMQClient.php
+++ rabbitmq/RabbitMQClient.php
@@ -4,13 +4,15 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+$client = new RabbitMQClient("testRabbitMQ.ini", 'testServer');
 if(isset($argv[1])){
 	$msg = $argv[1];
 }
 else{
 	$msg = "test message";
 }
+
+
 
 $request = array();
 $request['type'] = "login";
--- send.php
+++ send.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('10.242.125.55', 15672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('10.242.36.102', 5672, 'admin', 'admin');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
